6e66a0f681de736cb58583c3f07ef5f6b4120508
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-11-06 13:52:54 +0400

    feat: cors

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // подключаем логгер запросов
-app.use(corsSetting);
+// app.use(corsSetting);
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateUser, createUser);
 app.use(auth, usersRouter);
