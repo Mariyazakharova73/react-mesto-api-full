b4f089ae60333bc7f1c45c7ad7888bbb09d62497
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-11-06 16:56:51 +0400

    feat: crash

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
 const { errors } = require('celebrate');
 const usersRouter = require('./routes/usersRouter');
 const cardsRouter = require('./routes/cardsRouter');
@@ -15,13 +14,17 @@ const corsSetting = require('./middlewares/cors-setting');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-// пишем выше роутинга
-// app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // подключаем логгер запросов
 app.use(corsSetting);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateUser, createUser);
 app.use(auth, usersRouter);
